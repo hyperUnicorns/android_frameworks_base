fc64751fa15834196b1a3d3331e5d37b512f032c
Caleb Xu <calebcenter@live.com>
2017-06-18 17:50:21 -0400

    Revert "Lower minimum screenshot delay to 50ms"
    DU wisely lowered their minimum screenshot delay again. All is
    well in the universe, and this is unnecessary now.
    
    This reverts commit 10953eec33072dab8475f8444913f0458ae396cd.

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -1643,6 +1643,6 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private void checkSettings() {
         mScreenshotDelay = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SCREENSHOT_DELAY, 50);
+                Settings.System.SCREENSHOT_DELAY, 100);
     }
 }
